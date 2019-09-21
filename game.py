be5e471f085f8e2bf39c8fcd3ed7fd22bf97616b
Giorgos Vasilakis <vasilakisg2@gmail.com>
2019-09-20 22:23:24 -0700

    finished adding game rules

--- game.py
+++ game.py
@@ -4,7 +4,6 @@ import numpy as np
 class Game:
 
 	def __init__(self):
-
 		self.board = np.empty([4, 4])
 		self.board[:] = np.nan
 
@@ -19,26 +18,30 @@ class Game:
 
 
 	def move_left(self):
-
-		for row in self.board:
-			_ = self.leftRowOperation(row)
+		for i in range(len(self.board)):
+			self.board[i] = self.leftRowOperation(self.board[i])
 
 
 	def move_right(self):
+		for i in range(len(self.board)):
+			self.board[i] = self.rightRowOperation(self.board[i])
 
-		for row in self.board:
-			_ = self.rightRowOperation(row)
+
+	def move_up(self):
+		for j in range(len(self.board[0])):
+			self.board[:, j] = self.upColumnOperation(self.board[:, j])
 
 
-	# def move_up(self):
+	def move_down(self):
+		for j in range(len(self.board[0])):
+			self.board[:, j] = self.downColumnOperation(self.board[:, j])
 
-	# def move_down(self):
 
 	def ended(self):
 		return not np.isnan(self.board).any()
 
-	def generate_new(self):
 
+	def generate_new(self):
 		new_cell = np.random.randint(0, 4, 2)
 
 		while (not np.isnan(self.board[new_cell[0]][new_cell[1]])):
@@ -52,13 +55,6 @@ class Game:
 		prob = np.random.randint(0, 2, 1)
 		return prob * 2 + (1 - prob) * 4
 
-	# @static
-	# def modified_addition(a, b):
-
-	# 	if (a == b):
-	# 		return a + b
-	# 	else:
-	# 		return np.nan
 
 	@staticmethod
 	def leftRowOperation(row):
@@ -114,20 +110,91 @@ class Game:
 					if j < 0:
 						break
 			return row
+         
+		return addRow(row)
+
+
+	@staticmethod
+	def upColumnOperation(col):
+
+		def addCol(col):
+			col = compressCol(col)
+			for i in range(len(col) - 1):
+		            if col[i] == col[i + 1] and not np.isnan(col[i]):
+		                    col[i] *= 2
+		                    col[i + 1] = np.nan
+
+			col = compressCol(col)
+			return col
+
+		def compressCol(col):
+			for i in range(len(col)):
+		            if np.isnan(col[i]):
+		                    j = i+1
+		                    while (j<len(col)):
+		                            if not np.isnan(col[j]):
+		                                    col[i], col[j] = col[j], col[i]
+		                                    break
+		                            j  += 1
+		                    if j == len(col):
+		                    	break
+			return col
+
+		return addCol(col)
+
+
+	@staticmethod
+	def downColumnOperation(col):
+    	
+		def addCol(col):
+			col = compressCol(col)
+			for i in reversed(range(len(col))):
+				if col[i] == col[i-1] and not np.isnan(col[i]):
+					col[i] *= 2
+					col[i-1] = np.nan
+			col = compressCol(col)
+			return col
+
+
+		def compressCol(col):
+			for i in reversed(range(len(col))):
+				if np.isnan(col[i]):
+					j = i-1
+					while (j>=0):
+						if not np.isnan(col[j]):
+							col[i], col[j] = col[j], col[i]
+							break
+						j  -= 1
+					if j < 0:
+						break
+			return col
         
-		row = addRow(row)       
-		return row
+		col = addCol(col)       
+		return col
 
 
 game = Game()
 print (game.board)
 print('\n')
 while not game.ended():
-	game.move_left()
+	
+	move = np.random.randint(0, 4)
+	if move==0:
+		game.move_left()
+		print("Moving left")
+	elif move==1:
+		game.move_right()
+		print("Moving right")
+	elif move==2:
+		game.move_up()
+		print("Moving up")
+	else:
+		game.move_down()
+		print("Moving down")
+
 	game.generate_new()
 	print (game.board)
 	print('\n')
-
 print ('Game Over!')
 
 
